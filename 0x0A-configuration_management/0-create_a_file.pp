25a0e6a82a0e870f0ceb1e60dd2ac65a9e7fa608
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-14 14:36:25 -0700

    Using basics of Puppet CM

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# Create a file with Puppert into /tmp
+
+file {'school':
+  ensure => 'present',
+  content => 'I love Puppet',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  path => '/tmp/school',
+}
+	
