d092d4b7a7d518d363acee661009ebe50b322f9f
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-14 18:13:53 -0700

    puppet-lint considered

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,10 @@
 # Create a file with Puppert into /tmp
 
 file { '/tmp/school':
-  ensure => 'present',
+  ensure  => 'present',
   content => 'I love Puppet',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  path => '/tmp/school'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  path    => '/tmp/school'
 }
-	
