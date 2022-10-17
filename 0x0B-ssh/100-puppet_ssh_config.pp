800ae34025541da6f0c905f666340da1984b4c86
Hammed Tosin <hammedtosin188@gmail.com>
2022-10-16 19:25:54 -0700

    pup

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# edit config file
+
+exec { '/etc/ssh/ssh_config':
+path     => '/bin',
+command  => 'echo " IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> /etc/ssh/ssh_config',
+provider => 'shell',
+}
