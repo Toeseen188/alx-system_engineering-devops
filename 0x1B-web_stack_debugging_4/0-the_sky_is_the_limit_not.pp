93af503b4cfaa1808e4aa7cc64fd89c032e3e027
Tosin <hammedtosin188@gmail.com>
2023-05-19 00:22:35 -0700

    try

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,5 @@
-# increase the number of requests nginx can take
-
-# set ULIMIT
-exec {'increase nginx limit':
-  commad => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path   => '/usr/local/bin/:/bin/'
-  }
-
-# restart nginx
-exec {'nginx restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
-  }
+# Configure nginx to allow more requests
+exec { 'fix--for-nginx':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
